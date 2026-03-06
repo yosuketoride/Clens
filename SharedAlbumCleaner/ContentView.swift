27cde6884967481fcf86ce0de0dcdcc0acecee75
Yosuke Toride <yosuketoride@gmail.com>
2026-03-06 14:49:01 +0900

    Initial Commit

---
+++ SharedAlbumCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SharedAlbumCleaner
+//
+//  Created by 取出洋介 on 2026/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SharedAlbumCleaner/SharedAlbumCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedAlbumCleanerApp.swift
+//  SharedAlbumCleaner
+//
+//  Created by 取出洋介 on 2026/03/06.
+//
+
+import SwiftUI
+
+@main
+struct SharedAlbumCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
